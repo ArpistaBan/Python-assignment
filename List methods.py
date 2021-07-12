72a1983d34f3af6b1012d0acc6496b288ea989f1
ArpistaBan <87313166+ArpistaBan@users.noreply.github.com>
2021-07-12 19:13:00 +0530

    Add files via upload

---
+++ List methods.py
@@ -0,0 +1,40 @@
+avengers=["Iron man", "Captain America", "Hulk", "Thor", "Black Widow"]
+avengers.append("Hawkeye")
+print(avengers)
+fruits=["Kiwi", "Banana", "Mango"]
+fruits.clear()
+print(fruits)
+fruits=["Kiwi", "Banana", "Mango"]
+fruits.copy()
+print(fruits)
+fruits=["Kiwi", "Banana", "Mango", "Banana"]
+x=fruits.count("Banana")
+print(x)
+fruits=["Kiwi", "Banana", "Mango"]
+veggies=["Potato", "Tomato", "Onion"]
+fruits.extend(veggies)
+print(fruits)
+fruits=["Kiwi", "Banana", "Mango"]
+x=fruits.index("Kiwi")
+print(x)
+fruits=["Kiwi", "Banana", "Mango"]
+fruits.insert(2, "orange")
+print(fruits)
+fruits=["Kiwi", "Banana", "Mango"]
+fruits.pop(2)
+print(fruits)
+fruits=["Kiwi", "Banana", "Mango"]
+fruits.remove("Mango")
+print(fruits)
+fruits=["Kiwi", "Banana", "Mango"]
+fruits.reverse()
+print(fruits)
+fruits=["Kiwi", "Banana", "Mango"]
+fruits.sort()
+print(fruits)
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
