e6c538005f9c2b987b3787c116d90cf9f5293f8c
ArpistaBan <87313166+ArpistaBan@users.noreply.github.com>
2021-07-12 20:07:42 +0530

    Add files via upload

---
+++ Dictionary methods.py
@@ -0,0 +1,46 @@
+student = {
+  "name": "Arpista Banerjee",
+  "age": "19",
+  "location": "Kolkata",
+}
+student.clear()
+print(student)
+student = {
+  "name": "Arpista Banerjee",
+  "age": "19",
+  "location": "Kolkata",
+}
+x = student.copy()
+print(x)
+a = ('key1', 'key2', 'key3')
+b = 0
+dict1 = dict.fromkeys(x, y)
+print(dict1)
+student = {
+  "name": "Arpista Banerjee",
+  "age": "19",
+  "location": "Kolkata",
+}
+x = car.get("model")
+print(x)
+car = {
+  "brand": "Audi",
+  "model": "R8",
+  "year": 200
+}
+x = car.items()
+print(x)
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = car.keys()
+print(x)
+
+"""
+Created on Mon Jul 12 20:00:24 2021
+
+@author: arpis
+"""
+
