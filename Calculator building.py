14c333c9174981a5c2175b9a27590c42d0bec66c
ArpistaBan <87313166+ArpistaBan@users.noreply.github.com>
2021-07-12 19:57:22 +0530

    Add files via upload

---
+++ Calculator building.py
@@ -0,0 +1,54 @@
+# Write a program to build a simple calculator
+
+# Addition function
+def add(a, b):
+    return a + b
+
+# Subtraction function
+def subtract(a, b):
+    return a - b
+
+# Multiplication function
+def multiply(a, b):
+    return a * b
+
+# Division function
+def divide(a, b):
+    return a / b
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # Take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
+"""
+Created on Mon Jul 12 19:52:57 2021
+
+@author: arpis
+"""
+
