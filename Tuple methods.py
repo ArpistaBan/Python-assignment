215adfe50dc5616ae51d4a8569440632c87c441f
ArpistaBan <87313166+ArpistaBan@users.noreply.github.com>
2021-07-12 19:26:47 +0530

    Add files via upload

---
+++ Tuple methods.py
@@ -0,0 +1,13 @@
+# tuple methods
+tuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = tuple.count(5)
+print(x)
+tuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = tuple.index(8)
+print(x)
+"""
+Created on Mon Jul 12 19:17:24 2021
+
+@author: arpis
+"""
+
